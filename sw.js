cf736e260ba4bc4c633473eca5ac5aba363b700b
ismam3 <ismamlabib3@gmail.com>
2022-04-24 04:50:20 +0600

    A frame environment is made for offline

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install",e=>{
     e.waitUntil(
         caches.open("static").then(cache=>{
-            return cache.addAll(["./","./logo/192.png","./logo/512.png","./models/college24.glb","./models/mesh8.glb","./advertisement.png"])
+            return cache.addAll(["./","./logo/192.png","./logo/512.png","./models/college24.glb","./models/mesh8.glb","./advertisement.png","./aframe_environment.min.js"])
         })
     )
 });
